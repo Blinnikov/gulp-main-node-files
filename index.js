b8803f71c595abe5d6b3c89d2e48e50a639bdc51
Blinnikov <igor.blinnikov@gmail.com>
2017-05-26 14:50:32 +0300

    feat: support configurable options feat: support main-overrides in config

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
 var fs = require('fs');
 
-const packageJsonPath = './package.json';
-const nodeModulesPath = './node_modules';
+const defaultConfig = {
+	packageJsonPath: './package.json',
+	nodeModulesPath: './node_modules'
+};
+
 const defaultMainFile = 'index.js';
 
-function getMainNodeFiles() {
-    const packageJson = _getPackageJson(packageJsonPath);
-
-    return Object.keys(packageJson.dependencies)
-        .map(key => _getMainPackageFile(`${nodeModulesPath}/${key}`));
+function getMainNodeFiles(options) {
+	const config = Object.assign(defaultConfig, options);
+	const packageJson = _getPackageJson(config.packageJsonPath);
+	
+	var result = [];
+	
+	if(!packageJson.dependencies) {
+		return result;
+	}
+	
+	Object
+		.keys(packageJson.dependencies)
+		.forEach(key => {
+			if(config.overrides && config.overrides[key]) {
+				const overridenPaths = _getOverridenPaths(config, key);
+				result.push.apply(result, overridenPaths);
+			} else {
+				var defaultMainPackageFile = _getMainPackageFile(`${config.nodeModulesPath}/${key}`);
+				result.push(defaultMainPackageFile);
+			}
+	});
+	
+	return result;
 }
 
 function _getMainPackageFile(modulePath) {
-    var packageJson = _getPackageJson(modulePath + '/package.json');
-    return modulePath + "/" + (packageJson.main || defaultMainFile);
+	var packageJson = _getPackageJson(`${modulePath}/package.json`);
+	return `${modulePath}/${packageJson.main || defaultMainFile}`;
 }
 
 function _getPackageJson(path) {
-    const file = fs.readFileSync(path);
-    return JSON.parse(file.toString());
+	const file = fs.readFileSync(path);
+	if(!file) {
+		throw new Error('package.json not found');
+	}
+	
+	return JSON.parse(file.toString());
+}
+
+function _getOverridenPaths(config, key) {
+	const mainOverrides = config.overrides[key];
+	if(Array.isArray(mainOverrides)) {
+		return mainOverrides.map(path => `${config.nodeModulesPath}/${key}/${path}`);
+	} else {
+		return [`${config.nodeModulesPath}/${key}/${mainOverrides}`];
+	}
 }
 
 module.exports = getMainNodeFiles;
